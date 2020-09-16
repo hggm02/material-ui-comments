f1d51a6e02219ac5a430e56132fc0370e8b215bc
Mehmet Koseoglu <hggm02@gmail.com>
2020-09-16 16:03:42 +1000

    comment box added

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -10,9 +10,11 @@ import {
   Typography,
   Card,
   IconButton,
+  TextField,
 } from "@material-ui/core";
 import Faker from "faker";
 import DeleteIcon from "@material-ui/icons/Delete";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,45 +22,72 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
     maxWidth: "60ch",
   },
+  card: {
+    maxHeight: 200,
+    overflow: "auto",
+    maxWidth: "65ch",
+  },
   fonts: {
     fontWeight: "bold",
   },
   inline: {
     display: "inline",
   },
+  form: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: 450,
+    },
+  },
 }));
 
 const Comment = ({ comments }) => {
   const classes = useStyles();
   return (
-    <Card style={{ maxHeight: 200, overflow: "auto", maxWidth: "70ch" }}>
-      <List className={classes.root}>
-        {comments.map((comment) => {
-          console.log("Comment", comment);
-          return (
-            <React.Fragment key={comment.id}>
-              <ListItem key={comment.id} alignItems="flex-start">
-                <ListItemAvatar>
-                  <Avatar alt="avatar" src={Faker.image.avatar()} />
-                </ListItemAvatar>
+    <Card className={classes.root}>
+      <Card className={classes.card}>
+        <List className={classes.root}>
+          {comments.map((comment) => {
+            console.log("Comment", comment);
+            return (
+              <React.Fragment key={comment.id}>
+                <ListItem key={comment.id} alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="avatar" src={Faker.image.avatar()} />
+                  </ListItemAvatar>
 
-                <ListItemText
-                  primary={
-                    <Typography className={classes.fonts}>
-                      {comment.name}
-                    </Typography>
-                  }
-                  secondary={comment.body}
-                />
-                <IconButton edge="end" aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              </ListItem>
-              <Divider Light />
-            </React.Fragment>
-          );
-        })}
-      </List>
+                  <ListItemText
+                    primary={
+                      <Typography className={classes.fonts}>
+                        {comment.name}
+                      </Typography>
+                    }
+                    secondary={comment.body}
+                  />
+                  <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItem>
+                <Divider Light />
+              </React.Fragment>
+            );
+          })}
+        </List>
+      </Card>
+      <form className={classes.form} noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          width="100%"
+          InputProps={{
+            endAdornment: (
+              <IconButton edge="end" aria-label="submit">
+                <ArrowUpwardIcon />
+              </IconButton>
+            ),
+          }}
+        />
+      </form>
     </Card>
   );
 };
